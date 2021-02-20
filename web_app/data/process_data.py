71e90d5ac436510b82a96676248772d5de4b2a13
Joshua Furtado <joshua.s.furtado@gmail.com>
2021-02-19 19:42:08 -0800

    Put ETL pipeline in process_data.py file

--- web_app/data/process_data.py
+++ web_app/data/process_data.py
@@ -1,16 +1,75 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Function to load datasets and merge them together.
+    Args:
+        messages_filepath (str): file path to messages csv data
+        categories_filepath (str): file path to categories csv data
+    Returns:
+        df (DataFrame): dataframe containing data merged from the messages and categories datasets
+    """
+    messages = pd.read_csv(messages_filepath)  # load messages dataset
+    categories = pd.read_csv(categories_filepath)  # load categories dataset
+
+    df = messages.merge(categories, on='id')  # merge datasets using inner join
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """Function that clean dataframe df.
+    Args:
+        df (DataFrame): dataframe containing data merged from the messages and categories datasets
+    Returns:
+        df (DataFrame): cleaned df dataframe
+    """
+
+    # Split categories into separate category columns
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = df['categories'].iloc[0]
+    # use this row to extract a list of new column names for categories
+    category_colnames = pd.Series(
+        row.split(';')).replace('-\d', '', regex=True)
+    categories.columns = category_colnames  # rename the columns of `categories`
+
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].replace('\D+', '', regex=True)
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # Replace categories column in df with new category columns
+    # drop the original categories column from `df`
+    df.drop('categories', axis=1, inplace=True)
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # Drop duplicates
+    df.drop_duplicates(inplace=True)
+    # replace label 2 values with 1 for category related
+    df['related'].replace(2, 1, inplace=True)
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Function that clean dataframe df.
+    Args:
+        df (DataFrame): cleaned dataframe containing messages and categories data
+        database_filename (str): database filename
+    Returns:
+        None
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('cleaned_data', engine, if_exists='replace', index=False)
 
 
 def main():
@@ -24,18 +83,18 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
